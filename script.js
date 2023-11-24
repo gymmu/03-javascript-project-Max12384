1e032434ef468b6a9bbed7bf9ca5393ae07a6577
Max Favor <e282234@edu.sbl.ch>
2023-11-24 08:19:30 +0100

    Aufgabe 1-2

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+
 export function aufgabe01(args) {
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    if (currentElement === "e"){
+    result.push("")}
+    else if(currentElement === "E"){
+    result.push("")}
+    else {
+      result.push(currentElement)
+    }
   }
   return result.join("")
 }
 
-export function aufgabe (args) {
+export function aufgabe02 (args) {
   const input = args
   const result = []
   
+  for (let i = 0; i < input.length; i++) {
+    let currentElement = input[i]
+    currentElement = currentElement.toUpperCase()
+    result.push(currentElement
+    )
+  }
   return result.join("")
 }
