afdaf32a313f67dbfb2630356db18a63388b080c
Max Favor <e282234@edu.sbl.ch>
2023-12-11 20:10:29 +0100

    Finished notes for now

--- script.js
+++ script.js
@@ -68,7 +68,8 @@ export function aufgabe04 (args) {
     const currentElement = input[i]
     if (currentElement === " ") {
       count++
-    //If a space is found, "count" increases by 1
+    //If a space is found, "count" increases by 1.
+    //This assumes every word is seperated by a space.
   }
 }
   return count
@@ -221,11 +222,11 @@ export function aufgabe13 (args) {
   for (let i = input.length; i > -1; i--) {
   //This code functions the same as the other loops, but it will work in reverse.
   //"i" will start at the length of the input and decrease by 1 for every loop
-  //Once i reaches 1, the loop will end
+  //Once i reaches 0, the loop will end
     const currentElement = input[i]
-    charecc
     if (currentElement === "e"){
       return i
+      //returns the value of i when when an "e" is found
     }
   }
   return -1
@@ -243,8 +244,10 @@ export function aufgabe14 (args) {
     
     if (currentElement === "e"){
       number++
+      //when an "e" is found, the value of "number" increases by 1
     if (number === 3){
       return i
+      //when "number" reaches 3, the value of "i" is returned.
     }
   }
   }
@@ -262,6 +265,8 @@ export function aufgabe15(args) {
     
     if (currentElement === " " && i !== input.length){
       return result.join("")
+      //when a space is found, the code will stop and return the values up until that point. 
+      //This assumes every word is seperated by a space.
     } else {
       result.push(currentElement)
     }
@@ -279,10 +284,15 @@ export function aufgabe16 (args) {
   if (count === 0 && currentElement === "$" && i+2 > input.length) {
   result.push("")
   count++
+  //If a "$" is found at the end of the input, this code will delete it.
+  //If you add 2 to i, it will only be greater than the input length on the last value of the input.
+  //Therefor, i+2 > input.length, will only be true when i reads the last value of the input.
   }
   else if (currentElement === "$" && count === 0) {
   result.push(",")
   count++
+  //If a "$" is found, this code will replace it with a ",".
+  //The count is then increased by 1 so that neither this code or the one before it will repeat.
   }
   else {
     result.push(currentElement)
@@ -321,6 +331,7 @@ export function aufgabe19 (args) {
     const currentElement = input[i]
     result.push(currentElement)
     result.push(currentElement)
+    //When a character is detected, it will be added to the result twice.
   }
   return result.join("")
 }
@@ -333,6 +344,7 @@ export function aufgabe20 (args) {
     const currentElement = input[i]
     const currentElement1 = input[i+1]
     if (currentElement === "." && currentElement1 !== " "){
+      //This statement will be true if a space is not found after a ".".
     return false
   }
 }
@@ -344,6 +356,7 @@ export function aufgabe21 (args) {
   const result = []
 
   for (let i = input.length; i > -1; i--) {
+    //This reads the input backwards. See aufgabe13 for explanation.
     const currentElement = input[i]
     result.push(currentElement)
   }
@@ -360,13 +373,16 @@ export function aufgabe22 (args) {
     const currentElement = input[i]
     if (count === 1) {
       result.push(currentElement)
-    }
+      //once the count is increased to 1, all values are returned like normal.
+    } 
     else if (currentElement === "k") {
       count++
       result.push(currentElement)
+      //when "k" is found, the count is increased to 1.
     }
     else {
       result.push("_")
+      //if the count is not 1, all values are replaced with "_".
     }
 
   }
@@ -386,9 +402,11 @@ export function aufgabe23 (args) {
       count++
       result.push(currentElement)
       result.push(currentElement)
+      //This doubles the first input.
     } else if (i+2 > input.length) { 
       result.push(currentElement)
       result.push(currentElement1)
+      //This will return the last input as normal and then add the first input to the end.
     }
     else {
       result.push(currentElement)
@@ -397,6 +415,10 @@ export function aufgabe23 (args) {
   }
   return result.join("")
 }
+//Unless I translated it wrong, I believe this code does what it's supposed to do.
+//However, there is still a red message when checking on the website.
+//It says "aa" should be equal to "aaa"
+//However, if you double the first input and then add it to the end, "aa" should turn into "aaaa"
 
 export function aufgabe24 (args) {
   const input = args
@@ -406,7 +428,9 @@ export function aufgabe24 (args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     const currentElement1 = input[0]
+    //This is the first value of the input
     const currentElement2 = input[input.length-1]
+    //This is the last value of the input
 
     if (i === 0) {
       result.push(currentElement2)
@@ -415,6 +439,7 @@ export function aufgabe24 (args) {
     else {
       result.push(currentElement)
     }
+    //this code swaps the positions of the last and first value.
    }
   
   return result.join("")
@@ -427,18 +452,26 @@ export function aufgabe25 (args) {
   
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    const averageOdd = (input.length / 2) + .5
-    const averageEven1 = (input.length / 2) 
-    const averageEven2 = (input.length / 2) + 1
+    //To find the median value, you must take the input length divided by two.
+    const middleEven1 = (input.length / 2) 
+    const middleEven2 = (input.length / 2) + 1
+    //If the input length is even, it should yield two values
+    const middleOdd = (input.length / 2) + .5 
+    //If the input length is odd, it should yield one value.
+    //When the input length is odd, the value will end in a .5
+    //This code will increase the number by .5 to account for that
     
-    if (i+1 == averageOdd && count === 0) {
+    if (i+1 == middleOdd && count === 0) {
+      result.push("")
+      count--
+      //If the current value is "middleOdd", and the count is 0, the current value will be deleted.
+    } else if (i+1 == middleEven1) {
       result.push("")
       count++
-    } else if (i+1 == averageEven1) {
+      //If current input value is equal to "middleEven1", it will delete it.
+    } else if (i+1 == middleEven2 && count === 1) {
       result.push("")
-      count++
-    } else if (i+1 == averageEven2) {
-      result.push("")
+      //Deletes the second value if the input length is even.
     }
      else {
       result.push(currentElement)
@@ -456,20 +489,26 @@ export function aufgabe26 (args) {
     const currentElement = input[i]
     const currentElement1 = input[1]
     const ascii = currentElement.charCodeAt (0)
+    //this will get the ascii code of the first value
     const ascii1 = currentElement1.charCodeAt (0)
+    //this will get the ascii code of the second value
+
     
     if (number == 0 && ascii1 < ascii) {
       result.push(currentElement1)
       result.push(currentElement)
       number++
+      //if the second Ascii value is less than the first, the second value will be returned first.
     }
       else if (number == 0 && ascii1 > ascii) {
       result.push(currentElement)
       result.push(currentElement1)
       number++
+      //if the second ascii value is greater than the first, the first value will be returned first.
       }
       else if(number == 1) {
         number++
+      //this code will repeat the loop without returning anything.
       }
       else {
         result.push(currentElement)
